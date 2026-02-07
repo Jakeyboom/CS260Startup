18d2a4e99d333f542819f436c71e0e2af212dc43
Jake Schulz <jakeyboom@gmail.com>
2026-02-06 17:02:20 -0700

    Imported BrowserRouter and other app components into app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,20 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+//This is the main app component that is rendered in index.jsx.
+//Here, I will integrate the basic app componenets.
+
+import About from './about/about.jsx';
+import Calendar from './calendar/calendar.jsx';
+import DayView from './day/day.jsx';
+import Login from './login/login.jsx';
+import AddAssignment from './add_assignment/add_assignemnt';
+import EditAssignment from './edit_assignment/edit_assignment';
+import AddClass from './add_class/add_class';
+import EditClass from './edit_class/edit_class';
+
+import {BrowserRouter, NavLink, Route, Routes} from 'react-router-dom';
+
 export default function App() {
   return (
 
