45454c83d7b52dd400d5abffd052d248884bc282
Jake Schulz <jakeyboom@gmail.com>
2026-02-08 16:37:41 -0700

    Implemented DayView.jsx

--- src/day/day.jsx
+++ src/day/day.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavLink } from 'react-router-dom';
 
 export function DayView() {
     return(
-        <main>
-            Day View Page will be here!
-        </main>
+    <main> 
+        <label for="assignments-due-today">Assignments due on this day:</label>
+        <ul class="classes" id="assignments-due-today">
+            <li>
+                <a href="edit_assignment.html">Assignment 1</a>
+                 <a href="edit_class.html">Geography</a>
+            </li>
+            <li>
+                <a href="edit_assignment.html">Assignment 2</a>
+                <a href="edit_class.html">Geography</a>
+            </li>
+            <li>
+                <a href="edit_assignment.html">Assignment 3</a>
+                <a href="edit_class.html">Math</a>
+            </li>
+        </ul>                
+        
+        <form action="main.html" method="get" id="return-button-form">
+            <div id="main-buttons">
+                <NavLink to='/prioritizer' className="btn btn-primary btn">Return to Overview</NavLink>
+
+            </div>
+        </form>
+    </main>
     )
 }
