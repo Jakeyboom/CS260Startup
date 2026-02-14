686d94ee211c501dc1414ad70e5f30d6a62059d2
Jake Schulz <jakeyboom@gmail.com>
2026-02-14 15:02:34 -0800

    Created 2 very basic handlers for logging in and creating an account.  Only implementation is that they log the email and password in the console.

--- src/login-service.js
+++ src/login-service.js
@@ -1,7 +1,19 @@
 //Here, I will implement the login service that will allow a user to self authenticate.
 //Note that the temporary implementation will simply allow a user to login locally using a password, but in the future, this will be replaced with a more secure authentication method (some kind of hashing).
 
-export function login(username, password) {
-    //for now, this will just push the username and password to local storage and do a log.
-    console.log('Logging in with username:' + username + ', password: ' + password);
+export function login(email, password) {
+    //for now, this will just push the email and password to local storage and do a log.
+    console.log('Logging in with email:' + email + ', password: ' + password);
+    return true;
+
+    //TODO: Implement authentication here.
+    
 }
+
+export function createAccount(email, password) {
+    console.log('Creating account with email:' + email + ', password: ' + password);
+    return true;
+
+    //TODO: Implement account creation and verification here.
+}
+
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './login.css';
+import { login, createAccount } from '../login-service';
 import { useNavigate } from 'react-router-dom';
 
 export  function Login() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
     const navigate = useNavigate();
 
-    const handleLogin = () => {console.log("Login requested"); navigate("/prioritizer")};
-    const handleCreateAccount = () => {console.log("Create Account requested"); navigate("/prioritizer")};
-    const handleForgotPassword = () => {console.log("Forgot Password requrested"); navigate("/prioritizer")};
+    const handleLogin = (event) => {
+      event.preventDefault();
+      if(login(email, password)) {
+        navigate("/prioritizer");
+      } else {
+        //TODO: Implement error handling here.
+      };
+    };
+
+    //Here will be the Create Account handler
+    const handleCreateAccount = (event) => {
+      console.log("Create Account requested"); 
+      if(createAccount(email, password)) {
+        navigate("/prioritizer");
+      } else {
+        //TODO: Implement error handling here.
+      }
+    };
+
+    
+    const handleForgotPassword = (event) => {console.log("Forgot Password requrested"); navigate("/prioritizer")};
 
 
     return(
@@ -17,10 +39,10 @@ export  function Login() {
         (<i>Please Login or Signup</i>)
       </h2>
 
-      <form action='prioritizer' method="get"> 
+      <form action='prioritizer' method="get" onSubmit={handleCreateAccount}> 
           <div className="form-row">
             <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" placeholder="email@email.com"/>
+            <input type="email" id="email" name="email" placeholder="email@email.com" onChange={(e) => setEmail(e.target.value)}/>
           </div>
           <br/>
 
@@ -28,12 +50,12 @@ export  function Login() {
 
           <div className="form-row">
             <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" placeholder="passWord123" />
+            <input type="password" id="password" name="password" placeholder="passWord123" onChange={(e) => setPassword(e.target.value)}/>
           </div>
           
           <div id="login-buttons">
             <button type="button" className="btn btn-primary btn" onClick={handleLogin}>Login</button>
-            <button type="button" className="btn btn-primary btn" onClick={handleCreateAccount}>Create Account</button>
+            <button type="submit" className="btn btn-primary btn">Create Account</button>
             <button type="button" className="btn btn-primary btn" onClick={handleForgotPassword}>Forgot Password</button>
 
           </div>
