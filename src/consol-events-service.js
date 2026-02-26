6866a0910d1a459901f26adf7a7037efef54f69b
Jake Schulz <jakeyboom@gmail.com>
2026-02-26 13:12:39 -0700

    Implemented a non-working version of console-events-service.js using Simon React as a template

--- src/consol-events-service.js
+++ src/consol-events-service.js
@@ -1,3 +1,10 @@
+const ConsoleEvent = {
+    ACCOUNT_CREATED: 'ACCOUNT_CREATED',
+    CLASS_CREATED: 'CLASS_CREATED',
+    ASSIGNMENT_CREATED: 'ASSIGNMENT_CREATED',
+    
+}
+
 class ConsoleMessage {
     constructor(user, message, userClassName, userAssignmentName) {
         this.user = user;
@@ -6,3 +13,30 @@ class ConsoleMessage {
         this.userAssignmentName = userAssignmentName;
     }
 }
+
+class ConsoleEventNotifier {
+    consoleEvents = [];
+    handlers = [];
+
+    constructor() {
+        const userName = getCurrentUser();
+    }
+
+
+    addHandler(handler) {
+        this.handlers.push(handler);
+    }
+
+    removeHandler(handler) {
+        this.handlers = this.handlers.filter((h) => h !== handler);
+    }
+
+    broadcastEvent(user, message, userClassName, userAssignmentName) {
+        const consoleMessage = new ConsoleMessage(user, message, userClassName, userAssignmentName);
+        this.consoleEvents.push(consoleMessage);
+        this.handlers.forEach((handler) => handler(consoleMessage));
+    }
+}
+
+const ConsoleNotifier = new ConsoleEventNotifier();
+export {C}
