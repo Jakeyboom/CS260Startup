e311df3db663a981d75107ca8a22a9b67823bb44
Jake Schulz <jakeyboom@gmail.com>
2026-03-12 18:01:30 -0600

    Removed unused import in day.jsx

--- src/day/day.jsx
+++ src/day/day.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavLink, useParams } from 'react-router-dom';
-import { getCurrentUserClasses } from '../class-service.js';
 
 export function DayView() {
 
